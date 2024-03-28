ea11aec2f486eee92614aa30ae9461077085a923
Dong Joo, Choi (Tony) <80091614+baegopa-always@users.noreply.github.com>
2024-03-28 12:05:07 +0900

    [Silver V] Title: 분수찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,23 @@
+# 백준 - 분수찾기
+# 실버 5
+import sys
+input = sys.stdin.readline
+
+def find_numerator(x):
+    n = 1
+    while n * (n + 1) < x:
+        n += 1
+    return n
+
+x = int(input())
+if x == 1:
+    print("1/1")
+    exit()
+numerator = find_numerator(x * 2)
+sum_to_n = ((numerator) * (numerator - 1) // 2) + 1
+dif = x - sum_to_n
+
+if numerator % 2 == 0:
+    print(1+dif,"/",numerator-dif, sep = "")
+else:
+    print(numerator-dif,"/",1+dif, sep = "")
